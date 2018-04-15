075080379de2e9abed11cb6c4e4f8df40bc11ce3
LonnieQin <354060093@qq.com>
2018-04-15 20:41:37 +0800

    First Commit

--- FaceIDDemo/ViewController.swift
+++ FaceIDDemo/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
-
+import LocalAuthentication
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func unlock(_ sender: UIButton) {
+        let context = LAContext()
+        var error:NSError?
+        let reasonString = "I need to use your face ID to unlock the app"
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString) {[weak self] (succeed, error) in
+                DispatchQueue.main.async {
+                    if succeed {
+                        self?.titleLabel.text = "Unlocked"
+                    } else {
+                        let alert = UIAlertController(title: "Message", message: "Validation fail", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Got it", style: .cancel, handler: nil))
+                        self?.show(alert, sender: nil)
+                    }
+                }
+            }
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
